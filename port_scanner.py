0100f71954289c1618447e8289f2bf0874d91f08
Rudhra Koul <rudhrakoul@Rudhras-MacBook-Air.local>
2025-06-06 14:24:53 +0530

    formatted

--- port_scanner.py
+++ port_scanner.py
@@ -1,14 +1,8 @@
 from concurrent.futures import ThreadPoolExecutor
 import socket
-import time
-import argparse
 
 
-MAX_WORKERS = 100
-
-
-
-def generate_port_chunks(port_range):
+def generate_port_chunks(port_range,MAX_WORKERS):
     port_ranges = port_range.split('-')
     port_chunks= []
 
@@ -38,48 +32,15 @@ def scan(ip_address, port_chunk):
 
 
 
-def Workers(port_range):
-    port_ranges = port_range.split('-')
+def Workers(port):
+    port_ranges = port.split('-')
     start_port = int(port_ranges[0])
     end_port = int(port_ranges[1])
+ 
+    ports_per_worker = 10  # Number of ports each worker will handle
 
-    
+    port_range= end_port - start_port
+    max_workers = min(400, max(10,port_range // ports_per_worker))
 
-    
-
-def main():
-    parser = argparse.ArgumentParser(description="Fast Python Port Scanner")
-    parser.add_argument("-ip", "--ip_address", required=True, help="Target IP address to scan")
-    parser.add_argument("-p", "--port_range", required=True, help="Port range to scan (e.g., 20-80)")
-    args = parser.parse_args()
-
-    ip_address = args.ip_address
-    port_range = args.port_range
-
-    if (ip_address.count('.') != 3 or not all(part.isdigit() and 0 <= int(part) < 256 for part in ip_address.split('.'))):
-        print("Invalid IP address format. Please provide a valid IPv4 address.")
-        return
-    
-    if not (port_range.count('-') == 1 and all(part.isdigit() and 0 <= int(part) <= 65535 for part in port_range.split('-'))):
-        print("Invalid port range format. Please provide a valid range (e.g., 20-80).")
-        return
-
-
-
-    port_chunks = generate_port_chunks(port_range)
-
-    start_time = time.time()
-
-    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        executor.map(scan, [ip_address] * len(port_chunks), port_chunks)
-
-    end_time = time.time()
-    print(f"Scanning completed in {end_time - start_time:.2f} seconds")          
-      
-
-def Workers(port)
-
-
-
-if __name__ == "__main__":
-    main()      
+    return max_workers if max_workers > 0 else 1
+   
