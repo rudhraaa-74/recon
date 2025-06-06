28813cee43a7abe1ead1223130e4bb7ef1cb93c7
Rudhra Koul <rudhrakoul@Rudhras-MacBook-Air.local>
2025-06-06 14:26:08 +0530

    added main

---
+++ main.py
@@ -0,0 +1,43 @@
+from concurrent.futures import ThreadPoolExecutor
+from port_scanner import generate_port_chunks, scan, Workers
+import time
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Fast Python Port Scanner")
+    parser.add_argument("-ip", "--ip_address", required=True, help="Target IP address to scan")
+    parser.add_argument("-p", "--port_range", required=True, help="Port range to scan (e.g., 20-80)")
+    parser.add_argument("-w", "--workers", type=int, default=10, help="Number of worker threads (default: 10)")
+    parser.add_argument("-t", "--timeout", type=int, default=1, help="Socket timeout in seconds (default: 1)")
+    parser.add_argument("-s", "--stealth", help="Run in stealth mode (no output)")
+    args = parser.parse_args()
+
+    ip_address = args.ip_address
+    port_range = args.port_range
+
+    if (ip_address.count('.') != 3 or not all(part.isdigit() and 0 <= int(part) < 256 for part in ip_address.split('.'))):
+        print("Invalid IP address format. Please provide a valid IPv4 address.")
+        return
+    
+    if not (port_range.count('-') == 1 and all(part.isdigit() and 0 <= int(part) <= 65535 for part in port_range.split('-'))):
+        print("Invalid port range format. Please provide a valid range (e.g., 20-80).")
+        return
+
+    MAX_WORKERS = Workers(port_range)
+
+
+    port_chunks = generate_port_chunks(port_range, MAX_WORKERS)
+
+    start_time = time.time()
+
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        executor.map(scan, [ip_address] * len(port_chunks), port_chunks)
+
+    end_time = time.time()
+    print(f"Scanning completed in {end_time - start_time:.2f} seconds")          
+      
+
+
+if __name__ == "__main__":
+    main()      
+
